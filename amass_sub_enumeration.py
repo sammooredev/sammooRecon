eb781640e877965ea31baec6128ed34f8a22971e
Sam <52508285+sammooredev@users.noreply.github.com>
2020-04-02 15:15:50 -0700

    Delete amass_sub_enumeration.py

--- amass_sub_enumeration.py
+++
@@ -1,9 +0,0 @@
-import os
-
-#spawn amass process
-
-def amassSpawner():
-    os.system('/snap/bin/amass enum -timeout 60 -df domains.txt >> potential_subdomains/amass_potential_subdomains.txt')
-    
-amassSpawner()
-    
