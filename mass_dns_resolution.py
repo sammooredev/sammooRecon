8f4afabe6a8de105d26fe2f25d7d0f0b3d518e26
Sam <52508285+sammooredev@users.noreply.github.com>
2020-06-14 13:36:01 -0700

    Delete mass_dns_resolution.py

--- mass_dns_resolution.py
+++
@@ -1,8 +0,0 @@
-import os 
-
-def awk_sed_sort():
-    os.system("cat hosts_online_unsorted.txt | awk '{print $1}' | sed 's/.$//' | sort -u > hosts_online.txt")
-
-def exec_massdns():
-    RESOLVERS_PATH = "resolvers.txt"
-    os.system('/home/sam/massdns/bin/massdns -s 15000 -t A -o S -r ' + RESOLVERS_PATH + ' --flush -w /online_hosts/hosts_online_unsorted.txt /potential_subdomains/all_potential_subdomains.txt')
